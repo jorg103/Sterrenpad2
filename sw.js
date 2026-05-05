ed58797b43aeedb6240ddea367d2b33ef57ced91
jorg103 <jorg_103@hotmail.com>
2026-05-05 18:56:39 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,17 +1,10 @@
-// TitansPad Service Worker - minimaal, geen caching
-// Zorgt dat de app altijd de nieuwste versie laadt
-
+// TitansPad Service Worker - geen caching, altijd vers laden
 self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', event => {
-  // Verwijder ALLE oude caches
   event.waitUntil(
     caches.keys()
       .then(keys => Promise.all(keys.map(k => caches.delete(k))))
       .then(() => self.clients.claim())
   );
 });
-
-// Altijd netwerk gebruiken, nooit cache
-self.addEventListener('fetch', event => {
-  event.respondWith(fetch(event.request));
-});
+// Geen fetch handler = browser doet alles zelf, geen cache problemen
